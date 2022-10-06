2bf174f639b147ad300b3b49c6070d425b67e7e6
Andre Martins <andree.martins_@hotmail.com>
2022-10-06 14:06:23 -0300

    fixed update image

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -107,7 +107,20 @@ class EventController extends Controller
         try {
             DB::beginTransaction();
 
-            Event::findOrFail($request->id)->update($request->all());
+            $data = $request->all();
+
+            // Image Upload
+
+            if(
+                $request->hasFile('image') && $request->file('image')->isValid()) {
+                $requestImage = $request->image;
+                $extension = $requestImage->extension();
+                $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+                $request->image->move(public_path('img/events'), $imageName);
+                $data['image'] = $imageName;
+            }
+
+            Event::findOrFail($request->id)->update($data);
 
             DB::commit();
 
